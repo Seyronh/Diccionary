901fe1dd205233ed6558049e4322a6df688405b8
Seyronh <35349963+Seyronh@users.noreply.github.com>
2021-08-09 17:27:24 +0200

    Update basic.js

--- examples/basic.js
+++ examples/basic.js
@@ -1,10 +1,10 @@
 const diccionaries = require('./index.js')
-const diccionary = new diccionaries(true) //true => it will be lowercase
+const dictionary = new diccionaries(true) //true => it will be lowercase
 let exampletext = 'Nory was a Catholic because her mother was a Catholic, and Nory’s mother was a Catholic because her father was a Catholic, and her father was a Catholic because his mother was a Catholic, or had been.'
 diccionary.add(exampletext)
-console.log(diccionary.list)
-console.log(diccionary.maxid)
-console.log(diccionary.count)
-console.log(diccionary.maxcount)
-console.log(diccionary.convert(exampletext))
-console.log(diccionary.convertnormalized(exampletext))
+console.log(dictionary.list)
+console.log(dictionary.maxid)
+console.log(dictionary.count)
+console.log(dictionary.maxcount)
+console.log(dictionary.convert(exampletext))
+console.log(dictionary.convertnormalized(exampletext))
